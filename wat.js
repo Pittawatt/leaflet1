51e966874bfe69ed24bce08b2b61cc9e10112ff9
Pittawatt <124548012+Pittawatt@users.noreply.github.com>
2023-05-28 01:53:16 +0700

    Add files via upload

---
+++ wat.js
@@ -0,0 +1,146 @@
+var wat = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศาลพระสทาศิวะ",
+          "type": "ศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54040074937205,
+            13.747939424101347
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศาลท้าวมหาพรหม",
+          "type": "ศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54039552666148,
+            13.744339638925268
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พระแม่ลักษมี",
+          "type": "ศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54060081629848,
+            13.74497856578715
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศาลพระตรีมูรติ",
+          "type": "ศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54048051343392,
+            13.747929942649137
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระศรีมหาอุมาเทวี",
+          "type": "วัดกับศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52290141636223,
+            13.724179171595651
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศาลเจ้าแม่ประดู่",
+          "type": "ศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50854500217383,
+            13.739922915932638
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดเทพมณเฑียร",
+          "type": "วัด"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50242881066106,
+            13.75046641505908
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดโพธิ์",
+          "type": "วัด"
+        },
+        "geometry": {
+          "coordinates": [
+            100.49328405767363,
+            13.746475855439826
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดท่าพระ",
+          "type": "วัด"
+        },
+        "geometry": {
+          "coordinates": [
+            100.47352395451503,
+            13.733424977638307
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดมหาบุศย์",
+          "type": "วัดกับศาล"
+        },
+        "geometry": {
+          "coordinates": [
+            100.6071265763128,
+            13.714805998809013
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
